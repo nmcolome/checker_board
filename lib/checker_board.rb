73c5350d1e8c19383a06f9bae1574ea36ba128fa
NataliaColome <nmcolome@gmail.com>
2018-07-17 13:24:50 -0600

    Refactor

--- lib/checker_board.rb
+++ lib/checker_board.rb
@@ -1,28 +1,24 @@
 class CheckerBoard
 
   def build(size)
-    board = Array.new(size, "X") { Array.new(size, "X") }
-    board.each_index do |i|
-      if i.odd?
-        board[i].each_index do |j|
-          if j.even?
-            board[i][j] = " "
-          else
-            board[i][j]
-          end
-        end
-      else
-        board[i].each_index do |j|
-          if j.odd?
-            board[i][j] = " "
-          else
-            board[i][j]
-          end
-        end
-      end
-    end
+    board = create(size)
+    add_white_spaces(board)
     board.each { |row| row << "\n" }
     board.join("").strip
+  end
+  
+  def create(size)
+    Array.new(size, "X") { Array.new(size, "X") }
+  end
+  
+  def add_white_spaces(board)
+    board.each_index do |i|
+      if i.odd?
+        board[i].each_index { |j| board[i][j] = " " if j.even? }
+      else
+        board[i].each_index { |j| board[i][j] = " " if j.odd? }
+      end
+    end
   end
 
 end
